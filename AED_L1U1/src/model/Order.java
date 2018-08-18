e09557794f5d5bb3549654ae845cb4e3e4d128b9
Steven Montealegre <Steven Montealegre@LAPTOP-6U069POD>
2018-08-18 16:42:46 -0500

    i have changed into the Order class and i removed some classes.

--- AED_L1U1/src/model/Order.java
+++ AED_L1U1/src/model/Order.java
@@ -2,4 +2,48 @@ package model;
 
 public class Order {
 
+	
+	public static void integerMergeSort(int[] vector) {
+
+		int n = vector.length;
+		if (n < 2)
+			return;
+		int mid = n / 2;
+		int left[] = new int[mid];
+		int right[] = new int[n - mid];
+		for (int i = 0; i < mid; i++)
+			left[i] = vector[i];
+		for (int i = mid; i < n; i++)
+			right[i - mid] = vector[i];
+		integerMergeSort(left);
+		integerMergeSort(right);
+		merge(vector, left, right);
+
+	}
+
+	public static void merge(int arr[], int left[], int right[]) {
+		int nL = left.length;
+		int nR = right.length;
+		int i = 0, j = 0, k = 0;
+		while (i < nL && j < nR) {
+			if (left[i] <= right[j]) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[i];
+				j++;
+			}
+			k++;
+		}
+		while (i < nL) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+		while (j < nR) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
+	}
 }
