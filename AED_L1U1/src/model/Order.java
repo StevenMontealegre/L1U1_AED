40e81496b53d9d659b624833af647b23f5040575
Steven Montealegre <Steven Montealegre@LAPTOP-6U069POD>
2018-08-18 17:10:08 -0500

    I changed the Merge method.

--- AED_L1U1/src/model/Order.java
+++ AED_L1U1/src/model/Order.java
@@ -2,7 +2,8 @@ package model;
 
 public class Order {
 
-	
+	// MergeSort recursive method for integer values
+
 	public static void integerMergeSort(int[] vector) {
 
 		int n = vector.length;
@@ -17,11 +18,57 @@ public class Order {
 			right[i - mid] = vector[i];
 		integerMergeSort(left);
 		integerMergeSort(right);
-		merge(vector, left, right);
+		integerMerge(vector, left, right);
 
 	}
 
-	public static void merge(int arr[], int left[], int right[]) {
+	public static void integerMerge(int arr[], int left[], int right[]) {
+		int nL = left.length;
+		int nR = right.length;
+		int i = 0, j = 0, k = 0;
+		while (i < nL && j < nR) {
+			if (left[i] <= right[j]) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[i];
+				j++;
+			}
+			k++;
+		}
+		while (i < nL) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+		while (j < nR) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+
+	// double values version:
+
+	public static void doubleMergeSort(double[] vector) {
+
+		int n = vector.length;
+		if (n < 2)
+			return;
+		int mid = n / 2;
+		double left[] = new double[mid];
+		double right[] = new double[n - mid];
+		for (int i = 0; i < mid; i++)
+			left[i] = vector[i];
+		for (int i = mid; i < n; i++)
+			right[i - mid] = vector[i];
+		doubleMergeSort(left);
+		doubleMergeSort(right);
+		doubleMerge(vector, left, right);
+
+	}
+
+	public static void doubleMerge(double arr[], double left[], double right[]) {
 		int nL = left.length;
 		int nR = right.length;
 		int i = 0, j = 0, k = 0;
