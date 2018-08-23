04c0165c9fa7e84453e6804cf5bf66a2f09582e0
lhincapie0 <lauhincapie97@gmail.com>
2018-08-22 19:02:56 -0500

    The main window of the interface was created in JavaFX

--- AED_L1U1/src/application/RandomWindowController.java
+++ AED_L1U1/src/application/RandomWindowController.java
@@ -1,23 +1,49 @@
 package application;
 
-import java.awt.TextArea;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import view.MainView;
 
 public class RandomWindowController {
 
 	
+	private MainView main;
+	
+	
+	
+	@FXML 
+	private TextField minField;
+	@FXML 
+	private TextField maxField;
+	@FXML 
+	private TextField nField;
+	@FXML 
+	private Button generateBut;
+	@FXML
+	private CheckBox sortedBox;
+	@FXML
+	private CheckBox randomBox;
+	@FXML
+	private CheckBox invertedBox;
+	@FXML 
+	private CheckBox percetageBox;
+	
+	public RandomWindowController(MainView main)
+	{
+		this.main = main;
+	}
 	
 	@FXML
-	private TextArea min;
-	private TextArea max;
-	private TextArea n;
-	
-	
-	
-	public void generateRandom(ActionEvent event)
+	public void actionPerformed(ActionEvent event)
 	{
-		System.out.println("HOLA");
+		double min = Double.parseDouble(minField.getText());
+		double max = Double.parseDouble(maxField.getText());
+		int n = Integer.parseInt(nField.getText());	
+		
+		
 	}
+	
 }
